1cdfa0600f5a3f1341990c8ffbd677b543faabf0
IamParadoxic <Harrisonmcd1999@hotmail.com>
2016-02-06 12:53:36 -0800

    Removed some black space

--- src/org/usfirst/frc/team2557/robot/subsystems/MotorWinch.java
+++ src/org/usfirst/frc/team2557/robot/subsystems/MotorWinch.java
@@ -28,13 +28,6 @@ public class MotorWinch extends Subsystem {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
